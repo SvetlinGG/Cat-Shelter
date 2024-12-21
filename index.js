0c37a744384cff5fa86dee86e21edb833e3e0a9c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-12-21 14:07:09 +0100

    Connect style sheet with html files

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const http = require('http');
 
+const indexHtml = require('./views/home/index.html')
+const siteCss = require('./content/styles/site.css')
+
 const server = http.createServer((req, res) => {
-    res.write('Hello my first Node.js project');
+
+    if ( req.url === '/styles/site.css'){
+        res.write(200, {
+            'content-type': 'text/css'
+        });
+        res.write(siteCss);
+        return res.end();
+    }
+    
+    res.writeHead(200, {
+        "content-type": 'text/html'
+    });
+
+    switch (req.url) {
+        case '/':
+            res.write(indexHtml);
+            break;
+    
+        default:
+            break;
+    }
 
     res.end();
 });
