8922e599b55394a33147ff95b24d5eb2491eef4c
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-12-21 15:09:41 +0100

    Add cat views

--- index.js
+++ index.js
@@ -2,20 +2,53 @@ const http = require('http');
 
 const indexHtml = require('./views/home/index.html');
 const siteCss = require('./content/styles/site.css');
-const addBreed = require('./views/addBreed.html')
+const addBreed = require('./views/addBreed.html');
+const addCat = require('./views/addCat.html');
+//const catHtml = require('./views/cat.html')
+
+const cats = [
+    {
+        id: 1,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Pretty Kitty',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 2,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Navcho',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 3,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Sisa',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+    {
+        id: 4,
+        imageUrl: 'https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg=',
+        name: 'Garry',
+        breed: 'Bombay Cat',
+        description: 'Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.',
+    },
+];
 
 const server = http.createServer((req, res) => {
 
-    if ( req.url === '/styles/site.css'){
-        res.write(200, {
-            'content-type': 'text/css'
+    if ( req.url === '/styles/site.css.js'){
+        res.writeHead(200, {
+            'content-type': 'text/css',
         });
         res.write(siteCss);
         return res.end();
     }
     
     res.writeHead(200, {
-        "content-type": 'text/html'
+        "content-type": 'text/html',
     });
 
     switch (req.url) {
@@ -25,8 +58,12 @@ const server = http.createServer((req, res) => {
         case '/cats/add-breed':
             res.write(addBreed);
             break;
+        case '/cats/add-cat':
+            res.write(addCat);
+            break;
     
         default:
+            res.write(`<h1>Page Not Found!</h1>`);
             break;
     }
 
--- views/cat.html.js
+++ views/cat.html.js
@@ -1,4 +1,4 @@
-<li>
+module.exports = `<li>
     <img src="${cat.imageUrl}" alt="${cat.name}">
     <h3>${cat.name}</h3>
     <p><span>Breed: </span>${cat.breed}</p>
@@ -7,4 +7,4 @@
         <li class="btn edit"><a href="">Change Info</a></li>
         <li class="btn delete"><a href="">New Home</a></li>
     </ul>
-</li>
+</li>`
