2ba0d26981a88df8aab98880c92266db82fc2e23
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2024-12-21 13:43:04 +0100

    create server

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const http = require('http');
 
+const server = http.createServer((req, res) => {
+    res.write('Hello my first Node.js project');
+
+    res.end();
+});
+
+server.listen(1000, console.log('Server is listening on port 1000'));
